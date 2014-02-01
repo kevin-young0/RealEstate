3bf884ea4fc74a212b688efe7953bcded2e62aec
Kevin Young <kevin_young0@yahoo.com>
2014-01-31 20:00:53 -0600

    Fix errors with last commit and start RealEstate class

--- RealEstateBoardGame/src/realestateboardgame/GameBoard.java
+++ RealEstateBoardGame/src/realestateboardgame/GameBoard.java
@@ -1,13 +1,65 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package realestateboardgame;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
- *
+ * This class will build the game board for play
+ * 
  * @author Kevin
  */
 public class GameBoard {
+    
+    private int[] location;
+    private String[] gameSquare;
+    private String[] squareType;
+    private int[] locationCost;
+    private int boardSize;
+    
+    /**
+     * All the work for the game board is done in the constructor
+     * @throws IOException 
+     */
+    public GameBoard() throws IOException{
+        
+        URL url = getClass().getResource("dictionaryOne.txt");
+        
+        // first, determine the number of entries in the file to size the array..
+        BufferedReader br = 
+                new BufferedReader(new InputStreamReader(url.openStream()));
+        boardSize = 0;
+        while (br.ready()) {
+            boardSize++;
+            br.readLine();
+        }
+        br.close();
+        
+        location = new int[boardSize];
+        gameSquare = new String[boardSize];
+        squareType = new String[boardSize];
+        locationCost = new int[boardSize];
+        
+        //Populate the Array of entries
+       br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        int i = 0;
+        while (br.ready()) {
+            String str = br.readLine();
+            
+            String[] splits = str.split(",");
+            
+            //Creating the maps that will be accessed by the dice rolls
+            location[i] = Integer.parseInt(splits[0]);
+            gameSquare[i] = splits[1];
+            squareType[i] = splits[2];
+            locationCost[i] = Integer.parseInt(splits[3]);
+            
+            i++;
+        }
+    }
     
 }
--- RealEstateBoardGame/src/realestateboardgame/RealEstateBoardGame.java
+++ RealEstateBoardGame/src/realestateboardgame/RealEstateBoardGame.java
@@ -1,5 +1,9 @@
 package realestateboardgame;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * This is the Real Estate Board game it will play 1000 games
  * It will run until the player is out of money or 1000 dice rolls
@@ -7,14 +11,46 @@ package realestateboardgame;
  * @author Kevin
  */
 public class RealEstateBoardGame {
+
+    
     private int cashFlow;
     private int playerToken;
+    private int numberOfDiceRolls = 0;
+    private static final int NUMBEROFGAMES = 1000;
+    private int gameCount = 0;
+    private boolean newGame = true;
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         
+        RealEstateBoardGame realEstate = new RealEstateBoardGame();
         
+        try {
+            GameBoard gb = new GameBoard();
+        } catch (IOException ex) {
+            Logger.getLogger(
+                    RealEstateBoardGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //Keep track of each games dice count
+        int diceCount = 0;
+        
+        while(realEstate.gameCount < NUMBEROFGAMES){
+            
+            //instantiate a new game when needed
+            if(realEstate.newGame){
+                realEstate.cashFlow = 1500;
+                realEstate.playerToken= 0;
+                
+                realEstate.newGame = false;
+            }
+            
+            
+            realEstate.gameCount++;
+        }
     }
+    
+    
 }
